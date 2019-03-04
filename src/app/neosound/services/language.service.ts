1ef6e1f6022714ef57c454b412bf7d5e8ac20975
Andrei Matusevich <hijacker999@gmail.com>
2019-03-04 21:04:37 +0300

    filters and translation fix

--- src/app/neosound/services/language.service.ts
+++ src/app/neosound/services/language.service.ts
@@ -110,7 +110,7 @@ const translationsSp = {
   'Avg.': 'promedio',
   'Med.': 'Medio',
   'Keywords': 'Keywords',
-  'show only keywods': 'mostrar solo palabras clave',
+  'show only keywords': 'mostrar solo palabras clave',
 };
 const translationsRu = {
   'Error:': 'Ошибка:',
@@ -219,7 +219,7 @@ const translationsRu = {
   'Avg.': 'Средн.',
   'Med.': 'Медиана',
   'Keywords': 'Ключевые слова',
-  'show only keywods': 'Показать только ключевые слова',
+  'show only keywords': 'Показать только ключевые слова',
 };
 
 @Injectable()
