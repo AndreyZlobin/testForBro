99e23389cbdbcde7313df60f28cfa8db95e10164
Andrei Matusevich <hijacker999@gmail.com>
2019-04-16 01:33:15 +0300

    remove hardcoded common keywords

--- src/app/neosound/pages/dashboard/dashboard.component.ts
+++ src/app/neosound/pages/dashboard/dashboard.component.ts
@@ -206,7 +206,7 @@ export class DashboardComponent implements OnInit {
       }
     });
     this.filesService.getTagClowd({}).subscribe(data => {
-      this.keywords2 = comonKeywords;
+      // this.keywords2 = comonKeywords;
       this.keywords = Object.keys(data.keywords).map(key => {
         return {
           text: key,
@@ -489,6 +489,9 @@ export class DashboardComponent implements OnInit {
             }
           ]
         };
+      }
+      if (data.popularWords) {
+        this.keywords2 = data.popularWords;
       }
     });
   }
