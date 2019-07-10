5563b779eb4bef95938cfd1fb6c1f24ba3dc0a34
Andrei Matusevich <hijacker999@gmail.com>
2019-07-11 02:24:17 +0300

    gosha-generator on charts

--- src/app/neosound/pages/charts/chart-page/chart-page.component.ts
+++ src/app/neosound/pages/charts/chart-page/chart-page.component.ts
@@ -2,6 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { energy } from './mocks';
 import { FilesService } from '../../../services/files.service';
 import { LanguageService } from '../../../services/language.service';
+import ColorScheme from "color-scheme";
+import {HttpClient} from "@angular/common/http";
+
+
+const rgbToHex = rgb => {
+  let hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+    hex = "0" + hex;
+  }
+  return hex;
+};
+
+const fullColorHex = (r, g, b) => {
+  var red = rgbToHex(r);
+  var green = rgbToHex(g);
+  var blue = rgbToHex(b);
+  return red + green + blue;
+};
+
 
 @Component({
   selector: 'ngx-chart-page',
@@ -33,7 +52,13 @@ export class ChartPageComponent implements OnInit {
   minutesStat: any = {};
   apiStat: any = {};
 
-  constructor(private filesService: FilesService) { }
+  config = {};
+  colors = [];
+
+  constructor(
+    private filesService: FilesService,
+    private http: HttpClient
+  ) {  }
 
   ngOnInit() {
     this.filesService.getFileStats({}).subscribe(data => {
@@ -52,6 +77,41 @@ export class ChartPageComponent implements OnInit {
         this.initCharts();
     });
     this.initCharts();
+
+    this.http.get("assets/config/config.json").subscribe((data: any) => {
+      this.config = data;
+      const scheme = new ColorScheme();
+      const rgb = data.colors.secondary
+        .substring(4, data.colors.secondary.length - 1)
+        .replace(/ /g, "")
+        .split(",");
+      const hex = fullColorHex(rgb[0], rgb[1], rgb[2]);
+
+      const s = 'tetrade', d = 0.5, v = 'pastel';
+      scheme
+        .from_hex(hex)
+        .scheme(s)
+        .distance(d)
+        .variation(v);
+      this.colors = [`#${hex}`, ...scheme.colors().map(c => `#${c}`)];
+    });
+  }
+
+  getColors(s, d, v) {
+      const scheme = new ColorScheme();
+      const rgb = this.config.colors.secondary
+        .substring(4, this.config.colors.secondary.length - 1)
+        .replace(/ /g, "")
+        .split(",");
+      const hex = fullColorHex(rgb[0], rgb[1], rgb[2]);
+
+      scheme
+        .from_hex(hex)
+        .scheme(s)
+        .distance(d)
+        .variation(v);
+
+      return [`#${hex}`, ...scheme.colors().map(c => `#${c}`)];
   }
 
   initCharts() {
@@ -74,6 +134,7 @@ export class ChartPageComponent implements OnInit {
 
   setSankey() {
     this.option1 = {
+        color: this.colors,
         tooltip: {
             trigger: 'item',
             triggerOn: 'mousemove'
@@ -118,6 +179,7 @@ export class ChartPageComponent implements OnInit {
     //     });
     // });
     this.option2 = {
+      // color: [this.colors[0], this.colors[6], this.colors[1]],
       tooltip : {
         trigger: 'axis',
         axisPointer: {
@@ -196,6 +258,7 @@ export class ChartPageComponent implements OnInit {
     var data = [[0,0,5],[0,1,1],[0,2,0],[0,3,0],[0,4,0],[0,5,0],[0,6,0],[0,7,0],[0,8,0],[0,9,0],[0,10,0],[0,11,2],[0,12,4],[0,13,1],[0,14,1],[0,15,3],[0,16,4],[0,17,6],[0,18,4],[0,19,4],[0,20,3],[0,21,3],[0,22,2],[0,23,5],[1,0,7],[1,1,0],[1,2,0],[1,3,0],[1,4,0],[1,5,0],[1,6,0],[1,7,0],[1,8,0],[1,9,0],[1,10,5],[1,11,2],[1,12,2],[1,13,6],[1,14,9],[1,15,11],[1,16,6],[1,17,7],[1,18,8],[1,19,12],[1,20,5],[1,21,5],[1,22,7],[1,23,2],[2,0,1],[2,1,1],[2,2,0],[2,3,0],[2,4,0],[2,5,0],[2,6,0],[2,7,0],[2,8,0],[2,9,0],[2,10,3],[2,11,2],[2,12,1],[2,13,9],[2,14,8],[2,15,10],[2,16,6],[2,17,5],[2,18,5],[2,19,5],[2,20,7],[2,21,4],[2,22,2],[2,23,4],[3,0,7],[3,1,3],[3,2,0],[3,3,0],[3,4,0],[3,5,0],[3,6,0],[3,7,0],[3,8,1],[3,9,0],[3,10,5],[3,11,4],[3,12,7],[3,13,14],[3,14,13],[3,15,12],[3,16,9],[3,17,5],[3,18,5],[3,19,10],[3,20,6],[3,21,4],[3,22,4],[3,23,1],[4,0,1],[4,1,3],[4,2,0],[4,3,0],[4,4,0],[4,5,1],[4,6,0],[4,7,0],[4,8,0],[4,9,2],[4,10,4],[4,11,4],[4,12,2],[4,13,4],[4,14,4],[4,15,14],[4,16,12],[4,17,1],[4,18,8],[4,19,5],[4,20,3],[4,21,7],[4,22,3],[4,23,0],[5,0,2],[5,1,1],[5,2,0],[5,3,3],[5,4,0],[5,5,0],[5,6,0],[5,7,0],[5,8,2],[5,9,0],[5,10,4],[5,11,1],[5,12,5],[5,13,10],[5,14,5],[5,15,7],[5,16,11],[5,17,6],[5,18,0],[5,19,5],[5,20,3],[5,21,4],[5,22,2],[5,23,0],[6,0,1],[6,1,0],[6,2,0],[6,3,0],[6,4,0],[6,5,0],[6,6,0],[6,7,0],[6,8,0],[6,9,0],[6,10,1],[6,11,0],[6,12,2],[6,13,1],[6,14,3],[6,15,4],[6,16,0],[6,17,0],[6,18,0],[6,19,0],[6,20,1],[6,21,2],[6,22,2],[6,23,6]];
 
     this.option3 = {
+      // color: this.colors,
       tooltip: {
         position: 'top'
       },
@@ -264,6 +327,7 @@ export class ChartPageComponent implements OnInit {
     });
 
     this.option4 = {
+      // color: [this.colors[0], this.colors[6], this.colors[1]],
       tooltip : {
           trigger: 'axis',
           axisPointer: {
@@ -315,6 +379,8 @@ export class ChartPageComponent implements OnInit {
     const legenddata = this.apiStat.data.legend || [];
     const seriesdata = this.apiStat.data.series || [];
     this.option5 = {
+        // color: this.colors,
+        title: {text: 'light'},
         tooltip : {
           trigger: 'item',
           formatter: "{a} <br/>{b} : {c} ({d}%)"
@@ -394,6 +460,7 @@ export class ChartPageComponent implements OnInit {
 
     this.option6 = null;
     this.option6 = {
+      // color: [this.colors[0], this.colors[6], this.colors[1]],
         backgroundColor: '#ffffff',
         tooltip: {
             trigger: 'item',
@@ -460,6 +527,7 @@ export class ChartPageComponent implements OnInit {
     };
     const minX = Math.min(..._data[0]) < 1 ? -0.5 : 0;
     this.option7 = {
+      // color: [this.colors[0], this.colors[6], this.colors[1]],
         legend: {
             data: legendData
         },
