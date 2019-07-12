1c6a2cfeab7ac79305c0eb31256652f812cb6115
Andrei Matusevich <hijacker999@gmail.com>
2019-07-12 16:17:38 +0300

    charts default colors

--- src/app/neosound/pages/charts/calls-by-day-chart-line/calls-by-day-chart-line.component.ts
+++ src/app/neosound/pages/charts/calls-by-day-chart-line/calls-by-day-chart-line.component.ts
@@ -22,7 +22,7 @@ export class CallsByDayChartLineComponent implements OnInit {
   apiStat: any = {};
 
   config = {};
-  colors = [];
+  colors = ["#c23531", "#2f4554", "#61a0a8", "#d48265", "#91c7ae", "#749f83", "#ca8622", "#bda29a", "#6e7074", "#546570", "#c4ccd3"];
 
   @Input() set fileStatData(data) {
     this.fileStat = data;
