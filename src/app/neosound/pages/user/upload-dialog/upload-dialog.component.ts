b142d4206a2e1490907d70890c3ed04d594a63ab
Myasnikou Heorhi <mysnikovgeorgiy@gmail.com>
2019-07-31 13:27:35 +0300

    added

--- src/app/neosound/pages/user/upload-dialog/upload-dialog.component.ts
+++ src/app/neosound/pages/user/upload-dialog/upload-dialog.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit, OnDestroy, Input, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { UsersService } from '../../../services/users.service';
-import { FilesService } from '../../../services/files.service';
-import { MediaRecorderService } from '../../../services/media-recorder.service';
-import { Router } from '@angular/router';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap';
-import { timer, Subscription } from 'rxjs';
-import { UploadEvent, UploadFile } from 'ngx-file-drop';
-import { LanguageService } from '../../../services/language.service';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  ViewChild,
+  ElementRef,
+  EventEmitter,
+  Output
+} from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { UsersService } from "../../../services/users.service";
+import { FilesService } from "../../../services/files.service";
+import { MediaRecorderService } from "../../../services/media-recorder.service";
+import { AnalyticsService } from "../../../services/analytics.service";
+import { Router } from "@angular/router";
+import { BsModalRef, BsModalService } from "ngx-bootstrap";
+import { timer, Subscription } from "rxjs";
+import { UploadEvent, UploadFile } from "ngx-file-drop";
+import { LanguageService } from "../../../services/language.service";
 
 const makeId = () => {
-  let text = '';
+  let text = "";
   const possible =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (let i = 0; i < 10; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -20,18 +30,17 @@ const makeId = () => {
   return text;
 };
 
-
 @Component({
-  selector: 'ngx-upload-dialog',
-  templateUrl: './upload-dialog.component.html',
-  styleUrls: ['./upload-dialog.component.scss'],
+  selector: "ngx-upload-dialog",
+  templateUrl: "./upload-dialog.component.html",
+  styleUrls: ["./upload-dialog.component.scss"]
 })
 export class UploadDialogComponent implements OnInit, OnDestroy {
-  batchid = '1';
+  batchid = "1";
   form: FormGroup;
-  error = '';
+  error = "";
   modalRef: BsModalRef;
-  modalType: string = 'record';
+  modalType: string = "record";
   ticks = 0;
   private isRecording: boolean = false;
   private mediaRecorder: any;
@@ -40,8 +49,8 @@ export class UploadDialogComponent implements OnInit, OnDestroy {
   private sub: Subscription;
   public files: UploadFile[] = [];
   currentFileParams;
-  successMessage = '';
-  errorMessage = '';
+  successMessage = "";
+  errorMessage = "";
   isPlaying = false;
   fileBlob;
   audio: any;
@@ -51,11 +60,11 @@ export class UploadDialogComponent implements OnInit, OnDestroy {
   count = 20;
   intervalRef;
   filename;
-  @ViewChild('templateModal') templateModal: ElementRef;
-  @ViewChild('confirmModal') confirmModal: ElementRef;
+  @ViewChild("templateModal") templateModal: ElementRef;
+  @ViewChild("confirmModal") confirmModal: ElementRef;
   @Input() set showDialog(visible) {
     if (visible) {
-      setTimeout(() => this.showModal(this.templateModal, 'record'), 0);
+      setTimeout(() => this.showModal(this.templateModal, "record"), 0);
     } else {
       this.hideModal();
       this.discard();
@@ -69,17 +78,18 @@ export class UploadDialogComponent implements OnInit, OnDestroy {
     private modalService: BsModalService,
     private mediaRecorderService: MediaRecorderService,
     private filesService: FilesService,
+    private analyticsService: AnalyticsService
   ) {
     this.sub = this.mediaRecorderService.stop$.subscribe(record => {
       this.fileBlob = record;
       const name = (this.filename = `${this.getFormattedTime()}.wav`);
       const file = new File([record], name, {
-        type: `audio/wav`,
+        type: `audio/wav`
       });
       this.currentFileParams = {
         batchid: this.batchid,
         name,
-        file,
+        file
       };
     });
   }
@@ -92,7 +102,7 @@ export class UploadDialogComponent implements OnInit, OnDestroy {
     const h = today.getHours();
     const mi = today.getMinutes();
     const s = today.getSeconds();
-    return y + '-' + m + '-' + d + '_' + h + ':' + mi + ':' + s;
+    return y + "-" + m + "-" + d + "_" + h + ":" + mi + ":" + s;
   }
 
   ngOnInit() {
@@ -101,34 +111,39 @@ export class UploadDialogComponent implements OnInit, OnDestroy {
   }
 
   record() {
+    this.analyticsService.trackEvent("upload", "record");
     this.mediaRecorderService.initialize();
     this.mediaRecorderService.start();
   }
 
   play() {
+    this.analyticsService.trackEvent("upload", "play");
     this.audio = new Audio();
     this.audio.src = URL.createObjectURL(this.fileBlob);
     this.audio.load();
     this.audio.play();
-    this.audio.addEventListener('pause', () => {
+    this.audio.addEventListener("pause", () => {
       this.isPlaying = false;
     });
-    this.audio.addEventListener('ended', () => {
+    this.audio.addEventListener("ended", () => {
       this.isPlaying = false;
     });
     this.isPlaying = true;
   }
 
   stopPlaying() {
+    this.analyticsService.trackEvent("upload", "stop");
     this.audio && this.audio.pause();
     this.isPlaying = false;
   }
 
   stop() {
+    this.analyticsService.trackEvent("upload", "stop");
     this.mediaRecorderService.stop();
   }
 
   discard() {
+    this.analyticsService.trackEvent("upload", "discard");
     this.mediaRecorderService.reset();
     // this.currentFileParams = undefined;
     this.files = [];
@@ -138,48 +153,51 @@ export class UploadDialogComponent implements OnInit, OnDestroy {
   }
 
   attach() {
+    this.analyticsService.trackEvent("upload", "attach");
     this.stopPlaying();
     this.filename &&
       (this.currentFileParams.name =
-        this.filename.replace('.wav', '') + '.wav');
+        this.filename.replace(".wav", "") + ".wav");
     this.upload(this.fileBlob);
     this.hideModal();
     this.attached = true;
-    this.router.navigateByUrl('/user/files/reload');
+    this.router.navigateByUrl("/user/files/reload");
   }
 
   upload(record) {
+    this.analyticsService.trackEvent("upload", "upload");
     const uploadFile = new FormData();
     const user = this.userService.getUserLocal();
-    const username = (user && user.username) || 'fronttrust';
-    const companyid = (user && user.companyid) || '';
+    const username = (user && user.username) || "fronttrust";
+    const companyid = (user && user.companyid) || "";
 
-    uploadFile.append('batchid', this.batchid);
-    uploadFile.append('username', username);
-    uploadFile.append('companyid', companyid);
-    uploadFile.append('file', this.currentFileParams.file);
+    uploadFile.append("batchid", this.batchid);
+    uploadFile.append("username", username);
+    uploadFile.append("companyid", companyid);
+    uploadFile.append("file", this.currentFileParams.file);
     this.filesService.uploadFile(uploadFile).subscribe(
       res => {
         this.uploaded = true;
         this.successMessage =
-          'Successfully uploaded to the server: ' + this.currentFileParams && this.currentFileParams.name;
+          "Successfully uploaded to the server: " + this.currentFileParams &&
+          this.currentFileParams.name;
         this.filesService.processFile(this.getFileParams()).subscribe(
           v => {
             this.proccessed = true;
             const params = this.getFileParams();
             this.filesService.setQuickFileParams(params);
           },
-          e => (this.errorMessage = e.error.message),
+          e => (this.errorMessage = e.error.message)
         );
       },
-      e => (this.errorMessage = e.error.message),
+      e => (this.errorMessage = e.error.message)
     );
   }
 
   getFileParams() {
     return {
       batchid: this.batchid,
-      filename: this.currentFileParams.name,
+      filename: this.currentFileParams.name
     };
   }
 
@@ -187,17 +205,17 @@ export class UploadDialogComponent implements OnInit, OnDestroy {
     this.stopPlaying();
     const params = this.getFileParams();
     this.filesService.setQuickFileParams(params);
-    this.router.navigateByUrl('/guest/results');
+    this.router.navigateByUrl("/guest/results");
     return false;
   }
 
   showModal(ref, modalType, newModal = true) {
-    this.successMessage = '';
+    this.successMessage = "";
     this.modalType = modalType;
     if (newModal) {
       this.hideModal();
       this.modalRef = this.modalService.show(ref, {
-        class: 'modal-lg modal-xl'
+        class: "modal-lg modal-xl"
       });
     }
   }
@@ -210,23 +228,23 @@ export class UploadDialogComponent implements OnInit, OnDestroy {
   }
 
   submit() {
-    this.error = '';
+    this.error = "";
     const params = {
       username: this.form.value.username,
       firstname: this.form.value.firstname,
       lastname: this.form.value.lastname,
-      email: this.form.value.email,
+      email: this.form.value.email
     };
     this.userService
       .createUser(params)
-      .subscribe(() => this.router.navigateByUrl('/'));
+      .subscribe(() => this.router.navigateByUrl("/"));
   }
 
   private createForm() {
     this.form = new FormGroup({
-      emotion: new FormControl({ value: '' }, Validators.required),
-      age: new FormControl({ value: '' }),
-      gender: new FormControl({ value: '' })
+      emotion: new FormControl({ value: "" }, Validators.required),
+      age: new FormControl({ value: "" }),
+      gender: new FormControl({ value: "" })
     });
     this.patchForm();
   }
@@ -234,7 +252,7 @@ export class UploadDialogComponent implements OnInit, OnDestroy {
   patchForm() {
     if (this.form) {
       this.form.setValue({
-        emotion: 'anger',
+        emotion: "anger",
         age: false,
         gender: false
       });
@@ -252,14 +270,14 @@ export class UploadDialogComponent implements OnInit, OnDestroy {
           const params = {
             batchid: this.batchid,
             name: currentFile.name,
-            file: currentFile,
+            file: currentFile
           };
           this.currentFileParams = params;
           this.fileBlob = currentFile;
         };
         reader.onerror = error => {
-          console.log('Error: ', error);
-          this.successMessage = '';
+          console.log("Error: ", error);
+          this.successMessage = "";
         };
       });
     }
@@ -269,42 +287,38 @@ export class UploadDialogComponent implements OnInit, OnDestroy {
     const params = {
       batchid: this.batchid,
       name: files.item(0).name,
-      file: files.item(0),
+      file: files.item(0)
     };
     this.currentFileParams = params;
     this.fileBlob = files.item(0);
     this.files.push(files.item(0) as any);
   }
 
-  public fileOver(event) {
-  }
+  public fileOver(event) {}
 
-  public fileLeave(event) {
-  }
+  public fileLeave(event) {}
 
   getInfo() {
     this.count--;
     const params = this.getFileParams();
     this.filesService.listFileResults(params).subscribe(
       res => {
-        if (
-          this.count <= 0
-        ) {
+        if (this.count <= 0) {
           this.proccessed = true;
           clearInterval(this.intervalRef);
         }
       },
-      e => (this.errorMessage = e.error.message),
+      e => (this.errorMessage = e.error.message)
     );
   }
   process(uploadModal) {
     this.modalRef.hide();
     this.discard();
-    this.showModal(uploadModal, 'upload', true);
+    this.showModal(uploadModal, "upload", true);
   }
   interrupt(uploadModal) {
     this.modalRef.hide();
-    this.showModal(uploadModal, 'record', true);
+    this.showModal(uploadModal, "record", true);
   }
   ngOnDestroy() {
     this.stopPlaying();
