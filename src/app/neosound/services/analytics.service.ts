f8799f1dedfeefb0662754b7ee64a3b572142934
Myasnikou Heorhi <mysnikovgeorgiy@gmail.com>
2019-08-08 16:05:14 +0300

    fixed analitix

--- src/app/neosound/services/analytics.service.ts
+++ src/app/neosound/services/analytics.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from "@angular/core";
-import { Router, NavigationEnd } from "@angular/router";
+import { NavigationEnd, Router } from "@angular/router";
+import { Location } from "@angular/common";
+import { filter } from "rxjs/operators";
+import { environment } from "../../../environments/environment";
 
 declare let ga: Function;
 
 @Injectable()
 export class AnalyticsService {
-  constructor(public router: Router) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        ga('send', 'pageview', event.urlAfterRedirects);
-      }
-    });
+  enabled: boolean = true;
+  constructor(private location: Location, private router: Router) {
+    this.enabled = environment.production;
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        ga("send", { hitType: "pageview", page: this.location.path() });
+      });
   }
   //   eventCategory	text	yes	Typically the object that was interacted with (e.g. 'Video')
   // eventAction	text	yes	The type of interaction (e.g. 'play')
@@ -21,11 +26,13 @@ export class AnalyticsService {
     eventAction: string,
     eventValue: string = null
   ) {
-    ga("send", {
-      hitType: "event",
-      eventCategory,
-      eventAction,
-      eventValue
-    });
+    if (this.enabled) {
+      ga("send", {
+        hitType: "event",
+        eventCategory,
+        eventAction,
+        eventValue
+      });
+    }
   }
 }
