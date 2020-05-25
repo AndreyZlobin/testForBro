3a8b208f61aa95e635b531f18773c0ccf98c8f66
Heorhi Miasnikou <>
2020-05-25 07:28:12 +0300

    added new settings set

---
+++ src/app/neosound/pages/admin/organisation-settings/components/stopwords/setup-stopwords.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { OrganizationSettingsService } from "../../../../../services/organization-settings.service";
+import { LanguageService } from "../../../../../services/language.service";
+
+@Component({
+  selector: "app-setup-stopwords",
+  templateUrl: "./setup-stopwords.component.html",
+  styleUrls: ["./setup-stopwords.component.scss"],
+})
+export class SetupStopwordsComponent implements OnInit {
+  public isLoading: boolean = true;
+  @Output() changed = new EventEmitter<boolean>();
+  public rules: any[] = [];
+  constructor(
+    private organizationSettingsService: OrganizationSettingsService
+  ) {}
+
+  ngOnInit() {
+    this.organizationSettingsService
+      .getStopwordsSettings()
+      .subscribe((data) => {
+        if (data && data.settings) {
+          this.rules = data.settings.keyword;
+          this.isLoading = false;
+        }
+      });
+  }
+
+  t(v) {
+    return LanguageService.t(v);
+  }
+
+  public save() {
+    this.isLoading = true;
+    this.organizationSettingsService
+      .updateSettings("keyword", this.rules)
+      .subscribe((data) => {
+        this.isLoading = false;
+      });
+  }
+
+  public onItemAdd(tag, index): void {
+    let c = [];
+    const tagVal = (tag && tag.value) || tag;
+    if (tagVal === "") {
+      return;
+    }
+    this.rules[index].keywords = this.rules[index].keywords.filter(
+      (a) => a.value !== tagVal
+    );
+    const val = tagVal.split(",").map((v) => v.trim());
+    val.map((v) =>
+      c.push({
+        value: v,
+        display: v,
+      })
+    );
+    let deduplicate = new Set([
+      ...this.rules[index].keywords.map((i) => i.value),
+      ...c.map((i) => i.value),
+    ]);
+
+    let buff = Array.from(deduplicate)
+      .map((v) => {
+        return { value: v, display: v };
+      })
+      .sort((a, b) => a.value.localeCompare(b.value));
+    this.rules[index].keywords = buff;
+  }
+
+  onItemRemove(tag, index): void {
+    this.rules[index].keywords = this.rules[index].keywords.filter(
+      (t) => t.value !== tag.value
+    );
+  }
+
+  removeRule(index) {
+    this.rules = this.rules.filter((v, i) => i !== index);
+  }
+  addRule() {
+    this.rules.push({ result: "", keywords: [] });
+  }
+  
+}
